5b2c5f6298cbb1a2d0cb146d55ea0d23f3a767d2
DGMEFG <syz1729990469@163.com>
2024-06-18 02:02:01 +0800

    update
    Former-commit-id: 404b9d3a74e650d7d1694b28c950ab13d7e34ea8

--- bin/extract.py
+++ bin/extract.py
@@ -1,5 +1,6 @@
-import json, sys, torch, argparse, re
+import json, sys, torch, argparse, os
 from ner import get_entity
+from tqdm import tqdm
 from transformers import BertTokenizer, BertForSequenceClassification
 
 import nltk
@@ -13,11 +14,9 @@ model = BertForSequenceClassification.from_pretrained(config['pretrained_model']
 sys.path.append(config['root_dir'])
 from data.ere_data.data_utils import combine_entity
 
-rgcn_train, rgcn_val = config['gcn_train'], config['gcn_val']
 
-def extract(sentence: str):
+def extract(sentence: str, output_dir: str) -> None:
     # 首先使用 ner 提取所有的实体
-    print('extracting...')
     res = get_entity(sentence)
     tokens, ens = res[0], res[1:]
     # 两两组合实体，再用 ere 提取关系
@@ -32,16 +31,17 @@ def extract(sentence: str):
             with torch.no_grad():
                 outputs = model(inputs).logits
             # 输出置信度(0~1)
+            # 打印预测的类别以及置信度
             prdicted_label = torch.argmax(outputs, dim=-1)[0]
+            print(en1, en2, config["schema"][prdicted_label], torch.max(outputs).item())
             if config["schema"][prdicted_label] != "NA":
-                print(f'extracting relation between {en1["name"]} and {en2["name"]}')
-                print("relation: {}\nconfidence: {}\n".
-                        format(config["schema"][prdicted_label], torch.max(torch.softmax(outputs, dim=-1)[0]).item()))
                 # 随机 70% 概率写入训练集，30%写入验证集
+                if file is None:
+                    continue
                 if torch.rand(1).item() > 0.3:
-                    file = rgcn_train
+                    file = os.path.join(output_dir, 'gcn_train.txt')
                 else:
-                    file = rgcn_val
+                    file = os.path.join(output_dir, 'gcn_val.txt')
                 with open(file, 'a', encoding='utf-8') as f:
                     f.write(en1["name"] + '\t' + config["schema"][prdicted_label] + '\t' + en2["name"] + '\n')
         
@@ -52,18 +52,15 @@ def split_article(article: str):
     sentences = sent_tokenize(data)
     return sentences
 
-def article_extract(article: str):
-    # 从文章中提取实体和关系
-    sentences = split_article(article)
-    for sentence in sentences:
-        print('extracting from sentence:', sentence)
-        extract(sentence)
         
 def main(args):
     if args.sentence:
-        extract(args.sentence)
+        extract(args.sentence, args.output_dir)
     elif args.article:
-        article_extract(args.article)
+        sentences = split_article(args.article)
+        print('extracting article ...')
+        for sentence in tqdm(sentences):
+            extract(sentence, args.output_dir)
     else:
         print('please input sentence or article path') 
 
@@ -71,5 +68,8 @@ if __name__ == '__main__':
     paser = argparse.ArgumentParser()
     paser.add_argument('--sentence', type=str, default=None, help='input sentence')
     paser.add_argument('--article', type=str, default=None, help='input article path')
+    paser.add_argument('--output_dir', type=str, default=None, help='output path')
     args = paser.parse_args()
+    if args.output_dir is not None and not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
     main(args)
