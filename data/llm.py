3e3244da75b4234d20db4f5415ec4132443c8ff2
DGMEFG <syz1729990469@163.com>
2024-06-18 12:11:49 +0800

    add app.py

--- data/llm.py
+++ data/llm.py
@@ -4,30 +4,21 @@ nltk.download('punkt')
 from nltk.tokenize import sent_tokenize
 
 # 处理文本
-def main():
-    # 读取文本
-    with open("./data/raw.txt", 'r', encoding='utf-8') as f:
-        text = f.read()
-    # 分句
+def llm(prompt: str, text: str, step: int = 5):
     sentences = sent_tokenize(text)
     # 调用 ollama 的 chat 函数，每 5 个句子调用一次
     str = ""
     # 增加处理进度条
     print("Now llama is processing...")
-    for i in tqdm(range(0, len(sentences), 5)):
+    for i in tqdm(range(0, len(sentences), step)):
         # 句首添加设计的 prompt
-        prompt = "Output only the answer, and avoid unnecessary content as much as possible.Use the simplest vocabulary.Make the following text easier to extract relationships from, and avoid using pronouns. Ideally, use only one form of words with the same meaning.\n"
         # prompt += "".join(sentences[i:i+10])
         # 调用 ollama 的 chat 函数
         mes = [
-            {"role": "user", "content": prompt + "".join(sentences[i:i+5])},
+            {"role": "user", "content": prompt + '\n' + "".join(sentences[i:i+step])},
         ]
         res = ollama.chat(model="llama3:8b", messages=mes)
         # 输出结果
         str += res['message']['content']
     # 写入
-    with open("./data/llm.txt", 'w', encoding='utf-8') as f:
-        f.write(str)
-    print("Done!")
-if __name__ == '__main__':
-    main()
+    return str
